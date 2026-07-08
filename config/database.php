fc733a91c7c6287c0433573b8f6532764a3fc50b
Gabriel Retzlaff <gretzlaff13@gmail.com>
2026-07-07 21:31:21 -0300

    finalização de dados

--- config/database.php
+++ config/database.php
@@ -1,16 +1,26 @@
 <?php
 $host = 'localhost';
-$port = '3306';
-$dbname = 'atendlab';
-$user = 'root';
-$password = '';
+$porta = '3306';
+$banco = 'atendlab';
+$usuario = 'root';
+$senha = '';
+
 try {
- $pdo = new PDO(
- "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
- $user,
- $password
- );
- $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        "mysql:host={$host};port={$porta};dbname={$banco};charset=utf8mb4",
+        $usuario,
+        $senha
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
 } catch (PDOException $e) {
- die('Erro ao conectar com o banco de dados: ' . $e->getMessage());
+    exit('Erro ao conectar com o banco de dados.');
 }
